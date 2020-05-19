e440dcbd1320303dcc678b15d079c8c5950732ce
MaryamMosstoufi <maryam@zooxoos.com>
2020-05-19 13:49:01 -0400

    Task 2

--- index.js
+++ index.js
@@ -25,11 +25,12 @@ if (game["Home Team Goals"] > game["Away Team Goals"]) {
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
-
-    /* code here */
-
+function getFinals(data) {
+    let finalGames = data.filter(c => (c["Stage"] == "Final"));
+    return finalGames;
 };
+console.log(getFinals(fifaData));
+
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
